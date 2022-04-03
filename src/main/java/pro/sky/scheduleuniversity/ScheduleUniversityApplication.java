fc3e31e05aae8972cc0097ef4578e960a68bbe3b
Olga22S <22031992Les>
2022-04-04 01:08:27 +0300

    Initial commit

---
+++ src/main/java/pro/sky/scheduleuniversity/ScheduleUniversityApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.scheduleuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleUniversityApplication.class, args);
+    }
+
+}
